7f30682c208b430ab66fb7c27df14680f9437415
Adriano Alves <adrianoalvescru@gmail.com>
2021-01-17 14:25:51 -0300

    Add orders list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -3,8 +3,7 @@ import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/Header';
-import Home from './src/Home';
+import Routes from './src/Routes';
 
 export default function App() {
 
@@ -12,14 +11,12 @@ export default function App() {
     OpenSans_400Regular, 
     OpenSans_700Bold
   });
-
   if (!fontsLoaded) {
     return <AppLoading />;
   }
   return (
     <View style={styles.container}>
-        <Header />
-        <Home />
+        <Routes />
       <StatusBar style="auto" />
     </View>
   );
--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,16 +1,25 @@
+import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 
 function Header() {
+
+  const navigation = useNavigation();
+    const handleOnPress = () => {
+      navigation.navigate('Home');
+    }
   return (
+    <TouchableWithoutFeedback onPress={handleOnPress}>
     <View style={styles.container}>
         <Image source={require('../assets/logo.png')} />
       <Text style={styles.text}>DS Deliver</Text>
       
       <StatusBar style="auto" />
     </View>
+    </TouchableWithoutFeedback>
   );
 }
 
